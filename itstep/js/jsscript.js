c94028aa01b6b70844944ff7867946c732743f0d
kirsenko <46247205+kirsenko@users.noreply.github.com>
2019-05-24 22:17:59 +0300

    Update jsscript.js

--- itstep/js/jsscript.js
+++ itstep/js/jsscript.js
@@ -1,23 +1,20 @@
-$(document).ready(function () {
-    $("#header-slider").owlCarousel({
-        items: 1,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 3500,
-        autoplayHoverPause: true
-    });
-});
+$(document).ready(function() {
+  $("#header-slider").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3500,
+    autoplayHoverPause: true
+  });
 
-
-$(document).ready(function () {
-    $("#rw-slider").owlCarousel({
-        items: 3,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 3500,
-        autoplayHoverPause: true,
-        dots: false,
-        nav: true,
-        margin: 30
-    });
+  $("#rw-slider").owlCarousel({
+    items: 3,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3500,
+    autoplayHoverPause: true,
+    dots: false,
+    nav: true,
+    margin: 30
+  });
 });
