a7fdecdb8655c9561080de173a26e77632a34492
kirsenko <kirsenko8000@gmail.com>
2019-06-01 11:31:46 +0300

    html

--- step_1_project/js/script.js
+++ step_1_project/js/script.js
@@ -10,15 +10,15 @@ $(document).ready(function () {
             $(itemClass).each(function () {
                 console.log($(this).data('name'))
                 if ($(event.target).text() === $(this).data('name')) {
-                    $(this).show('slow');
-                } else $(this).hide('slow');
+                    $(this).show();
+                } else $(this).hide();
             })
 
         });
     }
 
     const ourServices = new Tabs('.our-services-header-tabs-btn', 'services-tabs-btn-active', '.our-services-articles-item');
-    const ourWork = new Tabs('.our-work-header-tabs-btn', 'work-tabs-btn-active', '.our-services-articles-item');
+    const ourWork = new Tabs('.our-work-header-tabs-btn', 'work-tabs-btn-active', '.our-work-gallery-item');
 
     // $(document).on('click', '.our-services-header-tabs-btn', (event) => {
 
