8aed48a8a67a0d118ad2616c10d84db4c21c4499
kirsenko <kirsenko8000@gmail.com>
2019-06-01 11:58:22 +0300

    Update script.js

--- step_1_project/js/script.js
+++ step_1_project/js/script.js
@@ -2,6 +2,11 @@
 
 $(document).ready(function () {
     function Tabs(tabClass, tabClassActive, itemClass) {
+
+        $(itemClass).each(function () {
+            $(this).css("order", `${Math.floor(Math.random()*50)}`);
+        })
+
         $(document).on('click', tabClass, (event) => {
 
             $(event.target).addClass(tabClassActive);
@@ -9,6 +14,7 @@ $(document).ready(function () {
 
             $(itemClass).each(function () {
                 console.log($(this).data('name'))
+                $(this).css("order", `${Math.floor(Math.random()*50)}`);
                 if ($(event.target).text() === $(this).data('name')) {
                     $(this).show();
                 } else $(this).hide();
@@ -16,6 +22,8 @@ $(document).ready(function () {
 
         });
     }
+    console.log($('.our-work-gallery-item'));
+
 
     const ourServices = new Tabs('.our-services-header-tabs-btn', 'services-tabs-btn-active', '.our-services-articles-item');
     const ourWork = new Tabs('.our-work-header-tabs-btn', 'work-tabs-btn-active', '.our-work-gallery-item');
