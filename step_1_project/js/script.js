1e4b8fa562025d136eadc9b8b636b10487bd3d87
kirsenko <kirsenko8000@gmail.com>
2019-06-02 20:48:37 +0300

    Update script.js

--- step_1_project/js/script.js
+++ step_1_project/js/script.js
@@ -1,17 +1,17 @@
 'use strict'
 
 $(document).ready(function () {
-    const ourServices = new Tabs('.our-services-header-tabs-btn', 'services-tabs-btn-active', '.our-services-articles-item');
-    const ourWork = new Tabs('.our-work-header-tabs-btn', 'work-tabs-btn-active', '.our-work-gallery-item');
+    const ourServices = new Tabs('.our-services-header-tabs-btn', 'services-tabs-btn-active', '.our-services-articles-item', 'name');
+    const ourWork = new Tabs('.our-work-header-tabs-btn', 'work-tabs-btn-active', '.our-work-gallery-item', 'name');
 
-    const sliderAboutTheHam = new Slider('.slider-controls-img', 'slider-controls-img-active', '.slide')
-    const aboutTheHam = new Tabs('.slider-controls-img', 'slider-controls-img-active', '.slide');
+    const sliderAboutTheHam = new Slider('.slider-controls-img', 'slider-controls-img-active', '.slide', 'name')
+    const aboutTheHam = new Tabs('.slider-controls-img', 'slider-controls-img-active', '.slide', 'name');
 
 
-    function Tabs(tabClass, tabClassActive, itemClass) {
-        const defaultTab = $(`${tabClass}.${tabClassActive}`).data('name')
+    function Tabs(tabClass, tabClassActive, itemClass, dataName) {
+        const defaultTab = $(`${tabClass}.${tabClassActive}`).data(dataName)
         $(itemClass).each(function () {
-            ($(this).data('name') === defaultTab || !defaultTab) ? $(this).show(): $(this).hide();
+            ($(this).data(dataName) === defaultTab || !defaultTab) ? $(this).show(): $(this).hide();
             $(this).css("order", `${Math.floor(Math.random()*50)}`);
         })
 
@@ -20,12 +20,12 @@ $(document).ready(function () {
 
             $(itemClass).each(function () {
                 $(this).css("order", `${Math.floor(Math.random()*50)}`);
-                ($(event.currentTarget).data('name') === $(this).data('name') || !$(event.currentTarget).data('name')) ? $(this).show(): $(this).hide();
+                ($(event.currentTarget).data(dataName) === $(this).data(dataName) || !$(event.currentTarget).data(dataName)) ? $(this).show(): $(this).hide();
             })
         });
     }
 
-    function Slider(tabClass, tabClassActive, itemClass) {
+    function Slider(tabClass, tabClassActive, itemClass, dataName) {
         let currentSlide = 0;
         $(tabClass).eq(currentSlide).addClass(tabClassActive);
         $(document).on('click', '.slider-controls-right', function () {
@@ -34,7 +34,7 @@ $(document).ready(function () {
             currentSlide = (currentSlide + 1) % $(tabClass).length;
             $(tabClass).eq(currentSlide).addClass(tabClassActive);
             $(itemClass).each(function () {
-                ($(this).data('name') === $(tabClass).eq(currentSlide).data('name')) ? $(this).show(): $(this).hide();
+                ($(this).data(dataName) === $(tabClass).eq(currentSlide).data(dataName)) ? $(this).show(): $(this).hide();
             })
         })
         $(document).on('click', '.slider-controls-left', function () {
@@ -43,7 +43,7 @@ $(document).ready(function () {
             currentSlide = (currentSlide - 1) % $(tabClass).length;
             $(tabClass).eq(currentSlide).addClass(tabClassActive);
             $(itemClass).each(function () {
-                ($(this).data('name') === $(tabClass).eq(currentSlide).data('name')) ? $(this).show(): $(this).hide();
+                ($(this).data(dataName) === $(tabClass).eq(currentSlide).data(dataName)) ? $(this).show(): $(this).hide();
             })
         })
     }
