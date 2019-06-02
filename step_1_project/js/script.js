2a1a1df67c0e6345e1f21d27adc2e374908cbc27
kirsenko <kirsenko8000@gmail.com>
2019-06-02 16:45:55 +0300

    global

--- step_1_project/js/script.js
+++ step_1_project/js/script.js
@@ -1,35 +1,24 @@
 'use strict'
 
 $(document).ready(function () {
+
     function Tabs(tabClass, tabClassActive, itemClass) {
-
-        $(tabClass).each(function () {
-            if ($(this).hasClass(tabClassActive) || !$(this).data('name')) {
-                console.log('+');
-            }
-        })
-
+        const defaultTab = $(`${tabClass}.${tabClassActive}`).data('name')
         $(itemClass).each(function () {
+            ($(this).data('name') === defaultTab || !defaultTab) ? $(this).show(): $(this).hide();
             $(this).css("order", `${Math.floor(Math.random()*50)}`);
-
         })
 
         $(document).on('click', tabClass, (event) => {
-            // console.log($(event.currentTarget))
             $(event.currentTarget).addClass(tabClassActive).siblings().removeClass(tabClassActive);
 
             $(itemClass).each(function () {
-                // console.log($(this).data('name'))
                 $(this).css("order", `${Math.floor(Math.random()*50)}`);
-                if ($(event.currentTarget).data('name') === $(this).data('name') || !$(event.currentTarget).data('name')) {
-                    $(this).show();
-                } else $(this).hide();
+                ($(event.currentTarget).data('name') === $(this).data('name') || !$(event.currentTarget).data('name')) ? $(this).show(): $(this).hide();
             })
-
         });
     }
 
-    // console.dir(Tabs)
     const ourServices = new Tabs('.our-services-header-tabs-btn', 'services-tabs-btn-active', '.our-services-articles-item');
     const ourWork = new Tabs('.our-work-header-tabs-btn', 'work-tabs-btn-active', '.our-work-gallery-item');
     const aboutTheHam = new Tabs('.slider-controls-img', 'slider-controls-img-active', '.slide');
