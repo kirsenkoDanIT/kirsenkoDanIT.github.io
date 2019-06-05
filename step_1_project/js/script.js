68c2e0c190bda343aa65aefb0dba11e3db1fd39f
kirsenko <kirsenko8000@gmail.com>
2019-06-05 11:20:51 +0300

    1

--- step_1_project/js/script.js
+++ step_1_project/js/script.js
@@ -1,147 +1,156 @@
 "use strict";
 
-$(document).ready(function() {
-  const ourServices = new Tabs(
-    ".our-services-header-tabs-btn",
-    "services-tabs-btn-active",
-    ".our-services-articles-item",
-    "name"
-  );
-  const ourWork = new GalleryTabs(
-    ".our-work-header-tabs-btn",
-    "work-tabs-btn-active",
-    ".our-work-gallery-item",
-    "name",
-    ".load-more"
-  );
-  const loadMore = new LoadMore(".our-work-gallery-item", ".load-more");
-  const aboutTheHamSlider = new Slider(
-    ".slider-controls-img",
-    "slider-controls-img-active",
-    ".slide",
-    "name"
-  );
-  const aboutTheHam = new Tabs(
-    ".slider-controls-img",
-    "slider-controls-img-active",
-    ".slide",
-    "name"
-  );
+$(document).ready(function () {
+    const ourServices = new Tabs(
+        ".our-services-header-tabs-btn",
+        "services-tabs-btn-active",
+        ".our-services-articles-item",
+        "name"
+    );
+    const ourWork = new GalleryTabs(
+        ".our-work-header-tabs-btn",
+        "work-tabs-btn-active",
+        ".our-work-gallery-item",
+        "name",
+        ".load-more"
+    );
+    const loadMore = new LoadMore(".our-work-gallery-item", ".load-more");
+    const aboutTheHamSlider = new Slider(
+        ".slider-controls-img",
+        "slider-controls-img-active",
+        ".slide",
+        "name"
+    );
+    const aboutTheHam = new Tabs(
+        ".slider-controls-img",
+        "slider-controls-img-active",
+        ".slide",
+        "name"
+    );
 
-  function Tabs(tabClass, tabClassActive, itemClass, dataName) {
-    const defaultTab = $(`${tabClass}.${tabClassActive}`).data(dataName);
-    $(itemClass).each(function() {
-      $(this).data(dataName) === defaultTab ? $(this).show() : $(this).hide();
-    });
-    $(document).on("click", tabClass, event => {
-      $(event.currentTarget)
-        .addClass(tabClassActive)
-        .siblings()
-        .removeClass(tabClassActive);
-      $(itemClass).each(function() {
-        $(event.currentTarget).data(dataName) === $(this).data(dataName)
-          ? $(this).show()
-          : $(this).hide();
-      });
-    });
-  }
-
-  function GalleryTabs(
-    tabClass,
-    tabClassActive,
-    itemClass,
-    dataName,
-    loadMoreBtnClass
-  ) {
-    const sliceCount = count => {
-      $(itemClass).hide();
-      $(`${itemClass}:hidden`)
-        .slice(0, count)
-        .show();
-    };
-    const defaultTab = $(`${tabClass}.${tabClassActive}`).data(dataName);
-    $(itemClass).each(function() {
-      $(this).data(dataName) === defaultTab || !defaultTab
-        ? $(this).show()
-        : $(this).hide();
-      $(this).css(
-        "order",
-        `${Math.floor(Math.random() * $(itemClass).length)}`
-      );
-    });
-
-    if (!defaultTab) {
-      sliceCount(12);
+    function Tabs(tabClass, tabClassActive, itemClass, dataName) {
+        const defaultTab = $(`${tabClass}.${tabClassActive}`).data(dataName);
+        $(itemClass).each(function () {
+            $(this).data(dataName) === defaultTab ? $(this).show() : $(this).hide();
+        });
+        $(document).on("click", tabClass, event => {
+            $(event.currentTarget)
+                .addClass(tabClassActive)
+                .siblings()
+                .removeClass(tabClassActive);
+            $(itemClass).each(function () {
+                $(event.currentTarget).data(dataName) === $(this).data(dataName) ?
+                    $(this).show() :
+                    $(this).hide();
+            });
+        });
     }
 
-    $(document).on("click", tabClass, event => {
-      $(event.currentTarget)
-        .addClass(tabClassActive)
-        .siblings()
-        .removeClass(tabClassActive);
-      $(itemClass).each(function() {
-        $(this).css(
-          "order",
-          `${Math.floor(Math.random() * $(itemClass).length)}`
-        );
-        $(event.currentTarget).data(dataName) === $(this).data(dataName) ||
-        !$(event.currentTarget).data(dataName)
-          ? $(this).show()
-          : $(this).hide();
-      });
+    function GalleryTabs(
+        tabClass,
+        tabClassActive,
+        itemClass,
+        dataName,
+        loadMoreBtnClass
+    ) {
+        const sliceCount = count => {
+            $(itemClass).hide();
+            $(`${itemClass}:hidden`)
+                .slice(0, count)
+                .show();
+        };
+        const defaultTab = $(`${tabClass}.${tabClassActive}`).data(dataName);
+        $(itemClass).each(function () {
+            $(this).data(dataName) === defaultTab || !defaultTab ?
+                $(this).show() :
+                $(this).hide();
+            $(this).css(
+                "order",
+                `${Math.floor(Math.random() * $(itemClass).length)}`
+            );
+        });
 
-      if (!$(event.currentTarget).data(dataName)) {
-        sliceCount(12);
-      }
+        if (!defaultTab) {
+            sliceCount(12);
+        }
 
-      $(event.currentTarget).data(dataName)
-        ? $(loadMoreBtnClass).hide()
-        : $(loadMoreBtnClass).show();
-    });
-  }
+        $(document).on("click", tabClass, event => {
+            $(event.currentTarget)
+                .addClass(tabClassActive)
+                .siblings()
+                .removeClass(tabClassActive);
+            $(itemClass).each(function () {
+                $(this).css(
+                    "order",
+                    `${Math.floor(Math.random() * $(itemClass).length)}`
+                );
+                $(event.currentTarget).data(dataName) === $(this).data(dataName) ||
+                    !$(event.currentTarget).data(dataName) ?
+                    $(this).show() :
+                    $(this).hide();
+            });
 
-  function LoadMore(itemClass, loadMoreBtnClass) {
-    $(document).on("click", loadMoreBtnClass, function() {
-      $(this).hide();
-      const timeout = setTimeout(() => {
-        $(`${itemClass}:hidden`)
-          .slice(0, 12)
-          .show();
-        $(`${itemClass}:hidden`).length ? $(this).show() : $(this).hide();
-        clearTimeout(timeout);
-      }, 500);
-    });
-  }
+            if (!$(event.currentTarget).data(dataName)) {
+                sliceCount(12);
+            }
 
-  function Slider(tabClass, tabClassActive, itemClass, dataName) {
-    let currentSlide = Math.floor(Math.random() * $(tabClass).length);
-    $(tabClass)
-      .eq(currentSlide)
-      .addClass(tabClassActive);
+            $(event.currentTarget).data(dataName) ?
+                $(loadMoreBtnClass).hide() :
+                $(loadMoreBtnClass).show();
+        });
+    }
 
-    $(document).click(event => {
-      currentSlide = $(`.${tabClassActive}`).index(tabClass);
-      $(tabClass)
-        .eq(currentSlide)
-        .removeClass(tabClassActive);
+    function LoadMore(itemClass, loadMoreBtnClass) {
+        $(document).on("click", loadMoreBtnClass, function () {
+            $(this).hide();
+            const timeout = setTimeout(() => {
+                $(`${itemClass}:hidden`)
+                    .slice(0, 12)
+                    .show();
+                $(`${itemClass}:hidden`).length ? $(this).show() : $(this).hide();
+                clearTimeout(timeout);
+            }, 500);
+        });
+    }
 
-      if ($(event.target).hasClass("slider-controls-right")) {
-        currentSlide = (currentSlide + 1) % $(tabClass).length;
-      } else if ($(event.target).hasClass("slider-controls-left")) {
-        currentSlide = (currentSlide - 1) % $(tabClass).length;
-      }
-
-      $(tabClass)
-        .eq(currentSlide)
-        .addClass(tabClassActive);
-      $(itemClass).each((i, item) => {
-        $(item).data(dataName) ===
+    function Slider(tabClass, tabClassActive, itemClass, dataName) {
+        let currentSlide = Math.floor(Math.random() * $(tabClass).length);
         $(tabClass)
-          .eq(currentSlide)
-          .data(dataName)
-          ? $(item).show()
-          : $(item).hide();
-      });
+            .eq(currentSlide)
+            .addClass(tabClassActive);
+
+        $(document).click(event => {
+            currentSlide = $(`.${tabClassActive}`).index(tabClass);
+            $(tabClass)
+                .eq(currentSlide)
+                .removeClass(tabClassActive);
+
+            if ($(event.target).hasClass("slider-controls-right")) {
+                currentSlide = (currentSlide + 1) % $(tabClass).length;
+            } else if ($(event.target).hasClass("slider-controls-left")) {
+                currentSlide = (currentSlide - 1) % $(tabClass).length;
+            }
+
+            $(tabClass)
+                .eq(currentSlide)
+                .addClass(tabClassActive);
+            $(itemClass).each((i, item) => {
+                $(item).data(dataName) ===
+                    $(tabClass)
+                    .eq(currentSlide)
+                    .data(dataName) ?
+                    $(item).fadeIn(1000) :
+                    $(item).hide();
+            });
+        });
+    };
+    $(document).on("click", ".main-menu li a", function (event) {
+        event.preventDefault();
+        const id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 1000);
     });
-  }
+
 });
