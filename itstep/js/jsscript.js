ed8fdab7da51b5bfa4be80c665222a7c72309b7c
kirsenko <kirsenko8000@gmail.com>
2019-06-07 12:33:26 +0300

    1

--- itstep/js/jsscript.js
+++ itstep/js/jsscript.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $("#header-slider").owlCarousel({
     items: 1,
     loop: true,
@@ -17,4 +17,27 @@ $(document).ready(function() {
     nav: true,
     margin: 30
   });
+
+  function Archor(archorClass) {
+    $(document).on("click", archorClass, function (event) {
+      event.preventDefault();
+      const id = $(this).attr('href'),
+        top = $(id).offset().top;
+      $('body,html').animate({
+        scrollTop: top
+      }, 1000);
+    });
+  }
+  const up = new Archor('.arrow');
+  const down = new Archor('.main-menu a');
+
+
+  $(document).on('click', 'button.see-more', function () {
+    $('#who_we_are').slideToggle();
+  })
+
+  $(document).on('scroll', () => {
+    ($(document).scrollTop() >= $(window).height()) ? $('.arrow').fadeIn(1000): $('.arrow').fadeOut(1000);
+  });
+
 });
