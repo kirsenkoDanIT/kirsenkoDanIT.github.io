280f811386bb31128cb7c6464ddf21a973028190
kirsenko <kirsenko8000@gmail.com>
2019-06-24 01:51:11 +0300

    2

--- hw1_advanced_html/js/script.js
+++ hw1_advanced_html/js/script.js
@@ -1,10 +1,11 @@
 "use strict";
 
 $(document).ready(function () {
+  $('.main-menu').css('display', '');
   $('.burger-menu').on('click', function () {
     $('.burger-menu__open').toggle();
     $('.burger-menu__close').toggleClass('burger-menu__close--flex');
-    $('.main-menu').toggle();
+    $('.main-menu').css('display') !== 'flex' ? $('.main-menu').css('display', 'flex') : $('.main-menu').css('display', '');
   });
 });
 //# sourceMappingURL=script.js.map
